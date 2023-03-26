b62a7d22bc5472194f73cdd0ba4893ab95721bd1
AndrasFejes22 <afejes22@gmail.com>
2023-03-26 20:45:35 +0200

    application.properties and pom updated

--- src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -26,6 +26,17 @@ public class Publisher {
 
     }
 
+    /*
+    public Publisher(Long id, String name, String addressLine1, String city, String state, String zip) {
+        this.id = id;
+        this.name = name;
+        this.addressLine1 = addressLine1;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+    */
+
     public Long getId() {
         return id;
     }
